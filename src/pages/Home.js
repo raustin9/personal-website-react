43aff2d26b2a68e6ab6f1c2ec14c1c5219e474b9
Alex Austin <raustin9@vols.utk.edu>
2021-01-12 11:47:12 -0500

    More styling to home page grid

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,20 @@ import { Link } from 'react-router-dom'
 import './styles/Home.css'
 import styled from 'styled-components'
 import * as FaIcons from 'react-icons/fa'
+import { IconContext } from 'react-icons/lib'
 
 const Contact = styled(Link)`
     text-decoration: none;
+`;
+
+const Text = styled.p`
+    
+    font-size: 1.8rem;
+    color: #355070;
+    
+    @media screen and (min-width: 960px) {
+        font-size: 2.3rem;
+    }
 `;
 
 const Music = styled(Link)`text-decoration: none;`;
@@ -27,41 +38,63 @@ export const Home = () => {
         <div className="home">
             
                 <div className="grid">
-                    <LinkedIn target="_blank" href="https://www.linkedin.com/in/ralexaustin9/" id="face" className="grid-item">
-                        <FaIcons.FaLinkedin />
+                    <LinkedIn target="_blank" href="https://www.linkedin.com/in/ralexaustin9/" id="face" className="grid-item linkedin">
+                        <IconContext.Provider value={{ className: 'Icon linkedin' }}>
+                            <FaIcons.FaLinkedin />
+                        </IconContext.Provider>
                     </LinkedIn>
 
                     <Education to="aboutme/education" id="education" className="grid-item">
-                        <FaIcons.FaSchool />
+                        <IconContext.Provider value={{ className: 'Icon' }}>
+                            <FaIcons.FaSchool />
+                        </IconContext.Provider>
+                        <Text>Education</Text>
                     </Education>
 
                     <Organizations to="/aboutme/organizations" id="organizations" className="grid-item">
-                        <FaIcons.FaSitemap />
+                        <IconContext.Provider value={{ className: 'Icon' }}>
+                            <FaIcons.FaSitemap />
+                        </IconContext.Provider>
+                        <Text>Organizations</Text>
                     </Organizations>
 
                     <AboutMe to="/aboutme/personalstatement" id="aboutme" className="grid-item">
-                        <FaIcons.FaUser />
+                        <IconContext.Provider value={{ className: 'Icon' }}>
+                            <FaIcons.FaUser />
+                        </IconContext.Provider>
+                        <Text>About Me</Text>
                     </AboutMe>
 
                     <Music to="/music" id="music" className="grid-item">
-                        <FaIcons.FaMusic />
+                        <IconContext.Provider value={{ className: 'Icon' }}>
+                            <FaIcons.FaMusic />
+                        </IconContext.Provider>
+
+                        <Text>Music</Text>
                     </Music>
 
                     <Hobbies to="/aboutme/hobbies" id="hobbies" className="grid-item">
-                        <FaIcons.FaPencilRuler />
+                        <IconContext.Provider value={{ className: 'Icon' }}>
+                            <FaIcons.FaPencilRuler />
+                        </IconContext.Provider>
+                    
+                        <Text>Hobbies</Text>
                     </Hobbies>
 
                     <Contact to="/contact" id="contact" className="grid-item">
-                        <FaIcons.FaEnvelope />
+                        <IconContext.Provider value={{ className: 'Icon' }}>
+                            <FaIcons.FaEnvelope />
+                        </IconContext.Provider>
+        
+                        <Text>Contact</Text>
                     </Contact>
 
-                    <Github target="_blank" href="https://github.com/raustin9" id="thissite" className="grid-item">
-                        <FaIcons.FaGithub />
+                    <Github target="_blank" href="https://github.com/raustin9" id="thissite" className="grid-item github">
+                        <IconContext.Provider value={{ className: 'Icon github' }}>
+                            <FaIcons.FaGithub />
+                        </IconContext.Provider>
                     </Github>
                 </div>
-            
-            
-
         </div>
     )
 }
